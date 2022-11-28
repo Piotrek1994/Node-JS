dec382ae4c7cb34564963d0acd35ae727696cbc9
Piorek1994 <pprus94@gmail.com>
2022-11-28 19:34:17 +0100

    ejs files

--- routes/shop.js
+++ routes/shop.js
@@ -12,8 +12,6 @@ router.get('/', (req, res, next) => {
 		prods: products,
 		pageTitle: 'Shop',
 		path: '/',
-		hasProducts: products.length > 0,
-		activeShop: true,
 		productCSS: true,
 		productCSS: true,
 		activeAddProduct: true
