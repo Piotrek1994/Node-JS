c4d1b11e8b9704428b5a87964ea5cd5ee08f467a
Piorek1994 <pprus94@gmail.com>
2022-11-29 19:10:05 +0100

    mvc styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,15 @@
 const express = require('express')
 
-const path = require('path')
+const productsController = require('../controllers/products')
 
-const rootDir = require('../util/path')
 
 const router = express.Router()
 
-const products = []
 
-router.get('/add-product', (req, res, next) => {
-	res.render('add-product', {
-		pageTitle: 'Add product',
-		path: '/admin/add-product',
-		formsCSS: true,
-		productCSS: true,
-		activeAddProduct: true
-	})
-})
+router.get('/add-product', productsController.getAddProduct)
 
-router.post('/add-product', (req, res, next) => {
-	products.push({ title: req.body.title })
-	res.redirect('/')
-})
 
-exports.routes = router
-exports.products = products
+
+router.post('/add-product', productsController.postAddProduct)
+
+module.exports = router
