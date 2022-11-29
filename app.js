a5777038cdc9de544a91188e121f9057791384df
Piorek1994 <pprus94@gmail.com>
2022-11-29 19:13:14 +0100

    app styling

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const errorController = require('./controllers/error')
+
 const app = express()
 
 
@@ -14,13 +16,11 @@ const adminRoutes= require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-	res.status(404).render('404', { pageTitle: 'Page not found' })
-})
+app.use(errorController.get404)
 
 app.listen(5000)
