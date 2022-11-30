5dd82b4fa52e3bb43a58c5acb3f2c4e24f72d39b
Piorek1994 <pprus94@gmail.com>
2022-11-30 14:14:21 +0100

    controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,17 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
+const express = require('express')
 
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop')
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex)
 
-router.get('/products')
+router.get('/products', shopController.getProducts)
 
-router.get('/cart')
+router.get('/cart', shopController.getCart)
 
-router.get('/checkout')
+router.get('/checkout', shopController.getCheckout)
 
-
-
-module.exports = router;
+module.exports = router
