0a27f171ea3681949f59db8e6361a6a97e2d3896
Piorek1994 <pprus94@gmail.com>
2022-11-30 13:58:40 +0100

    adding routes

--- routes/shop.js
+++ routes/shop.js
@@ -8,4 +8,12 @@ const router = express.Router();
 
 router.get('/', productsController.getProducts);
 
+router.get('/products')
+
+router.get('/cart')
+
+router.get('/checkout')
+
+
+
 module.exports = router;
